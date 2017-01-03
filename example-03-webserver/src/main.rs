f4498119dce4db5d3faa5892a9c32b747b5c8ae9
Sebastian Waisbrot <sebastian.waisbrot@avature.net>
2017-01-03 14:13:10 -0300

    read/write http

--- example-03-webserver/src/main.rs
+++ example-03-webserver/src/main.rs
@@ -3,9 +3,11 @@ extern crate tokio_minihttp;
 extern crate tokio_proto;
 extern crate tokio_service;
 
+use std::str;
 use std::io;
-use std::io::Read;
-use std::fs::File;
+use std::io::{Read, Write};
+use std::fs::{create_dir_all, File};
+use std::path::Path;
 
 use futures::future;
 use tokio_minihttp::{Request, Response, Http};
@@ -13,6 +15,38 @@ use tokio_proto::TcpServer;
 use tokio_service::Service;
 
 struct HelloWorld;
+
+impl HelloWorld {
+    fn read(&mut self, request: Request) -> Result<String, (u32, &str)> {
+        File::open(&request.path()[1..]).and_then(|ref mut f| {
+            let mut s = String::new();
+            f.read_to_string(&mut s).map(|_| s)
+        }).map_err(|e| {
+            match e.kind() {
+                io::ErrorKind::NotFound => (404, "Not Found"),
+                _ => (500, "Internal Server Error"),
+            }
+        })
+    }
+
+    fn write(&mut self, request: Request) -> Result<(), (u32, &str)> {
+        let p = request.path();
+        let path = Path::new(&p[1..]);
+        if let Some(parent) = path.parent() {
+            if parent.is_file() {
+                return Err((400, "Parent path is a file"))
+            }
+            if ! parent.is_dir() {
+                if let Err(_) = create_dir_all(parent) {
+                    return Err((500, "Failed to create directory"))
+                }
+            }
+        }
+        File::create(path).and_then(|mut f| {
+            f.write(request.body().as_slice()).map(|_| ())
+        }).map_err(|_| (500, "Internal Server Error"))
+    }
+}
 
 impl Service for HelloWorld {
     type Request = Request;
@@ -22,20 +56,14 @@ impl Service for HelloWorld {
 
     fn call(&mut self, request: Request) -> Self::Future {
         let mut resp = Response::new();
-        match File::open(&request.path()[1..]) {
-            Ok(ref mut f) => {
-                let mut s = String::new();
-                if let Err(_e) = f.read_to_string(&mut s) {
-                    resp.status_code(500, "Internal Server Error")
-                } else {
-                    resp.body(&*s)
-                }
-            },
-            Err(ref e) =>  match e.kind() {
-                io::ErrorKind::NotFound => resp.status_code(404, "Not Found"),
-                _ => resp.status_code(500, "Internal Server Error"),
-            }
+        let r = match request.method() {
+            "POST" => self.write(request).map(|_| "OK".to_owned()),
+            _ => self.read(request),
         };
+        match r {
+            Ok(e) => { resp.body(&*e); },
+            Err((status, message)) => { resp.status_code(status, message); },
+        }
         future::ok(resp)
     }
 }
