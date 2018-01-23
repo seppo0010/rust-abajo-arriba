4fcb29ba084e21d055180230de88759dba12349b
Sebastian Waisbrot <sebastian.waisbrot@brubank.com>
2018-01-22 21:02:37 -0300

    update dependencies

--- example-01-microservice-revstring/src/main.rs
+++ example-01-microservice-revstring/src/main.rs
@@ -3,7 +3,7 @@ extern crate service_fn;
 
 fn main() {
 	let addr = "127.0.0.1:12345".parse().unwrap();
-	line::service::serve(
+	line::serve(
 		addr,
 		|| {
 			Ok(service_fn::service_fn(|msg: String| {
